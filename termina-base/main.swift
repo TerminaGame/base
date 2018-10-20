2f2c3977b26d2ff56429ad54c35090501ab753d8
Marquis Kurt <software@marquiskurt.net>
2018-10-20 12:09:17 -0400

    Initial Commit

---
+++ termina-base/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  termina-base
+//
+//  Created by Marquis Kurt on 10/20/18.
+//  Copyright © 2018 Marquis Kurt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
