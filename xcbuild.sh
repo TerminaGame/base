5c4e1f6632b7f518ebdfd79035e2e1bcf9cff2f1
Marquis Kurt <software@marquiskurt.net>
2018-11-06 10:34:50 -0500

    Fix errors that may be caused by templating issues

--- xcbuild.sh
+++ xcbuild.sh
@@ -5,6 +5,6 @@ security create-keychain -p temps build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p temps build.keychain
 security import certificate.p12 -k build.keychain -P
-security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k temps build.keychain
 xcodebuild -scheme termina-base -project termina-base.xcodeproj -derivedDataPath dist build
 # xcodebuild -scheme termina-base -project termina-base.xcodeproj -derivedDataPath dist build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
