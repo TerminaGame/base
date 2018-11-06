db9511f0d034bc86ec2425ccd9050d2e69d8d300
Marquis Kurt <software@marquiskurt.net>
2018-11-06 10:48:54 -0500

    Add code sign location?

--- xcbuild.sh
+++ xcbuild.sh
@@ -4,7 +4,7 @@ echo $APPLE_DEV_CERTIFICATE | base64 --decode > certificate.p12
 security create-keychain -p temps build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p temps build.keychain
-security import certificate.p12 -k build.keychain -P $CERT_PASSWORD
+security import certificate.p12 -k build.keychain -P $CERT_PASSWORD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k temps build.keychain
 xcodebuild -scheme termina-base -project termina-base.xcodeproj -derivedDataPath dist build
 # xcodebuild -scheme termina-base -project termina-base.xcodeproj -derivedDataPath dist build CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
